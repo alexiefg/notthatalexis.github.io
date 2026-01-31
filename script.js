c5709a38f65d9b39e37635834810b5448f79b5a8
alexiefg <122645734+alexiefg@users.noreply.github.com>
2026-01-31 18:15:15 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.querySelector(".menu-toggle");
-  const nav = document.querySelector(".mobile-nav");
-
-  button.addEventListener("click", function () {
-    nav.style.display =
-      nav.style.display === "block" ? "none" : "block";
-  });
-});

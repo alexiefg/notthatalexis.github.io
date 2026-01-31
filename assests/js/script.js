4f0e0ce058e3ed2c718f0a621334d12367ee429d
alexiefg <122645734+alexiefg@users.noreply.github.com>
2026-01-31 18:15:59 -0500

    Create script.js

---
+++ assests/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".mobile-nav");
+
+  if (!button || !nav) return;
+
+  button.addEventListener("click", function () {
+    nav.classList.toggle("open");
+  });
+});
